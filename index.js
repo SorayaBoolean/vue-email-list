f61af3a0cfca938d5204c240594120a50217e6da
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-08-22 15:54:33 +0200

    printed 10 emails addresses but not as a list way

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = new Vue ({
         for (let i = 0; i < 10 ; i ++) {
 
             axios.get ('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => {
+            .then( response => {
         
                 this.generatedEmail += response.data.response 
                 console.log (response.data.response)
