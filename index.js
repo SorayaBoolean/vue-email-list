cc5e7d56ba0c569d4191ad304e805ae1b9d6d831
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-08-22 15:23:22 +0200

    Now I haveonly one email generate with a GET method by api. Now I have to create a cycle for generate and create 10 email addresses and print them in browser

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var app = new Vue ({
     el: '#app',
-
+    data: {
+        generatedEmail : ''
+    },
     mounted () {
         console.log ('Vue avviato')
 
+        axios.get ('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.generatedEmail = response.data.response 
+
+        })
     }
 
 })
